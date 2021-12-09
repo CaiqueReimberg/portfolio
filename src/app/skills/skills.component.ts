997779c6c9ade1302fc0cda2c0f979378b0af48a
Caique Reimberg <caiquecreimberg@gmail.com>
2021-12-09 20:07:45 -0300

    Atribuido valor a pagina de skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  public skills = [
+    {
+      title: 'Front-end',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum ipsa, numquam dolor nisi iusto.',
+    },
+    {
+      title: 'Back-end',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum ipsa, numquam dolor nisi iusto.',
+    },
+    {
+      title: 'Desktop',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum ipsa, numquam dolor nisi iusto.',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public currentSkill = {
+    title: 'Ola',
+    description: 'aqui eu falo sobre bla'
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public skillClicked(skill: any): void {
+    this.currentSkill = skill;
+  }
 }
